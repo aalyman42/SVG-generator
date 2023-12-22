a21d6d0fdc4b740b049a4443314b7b67c9d76ba3
aalyman42 <aalyman42@gmail.com>
2023-12-21 21:47:25 -0700

    each shape now has its own class

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Shapes = require("./lib/shapes");
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Circle = Shapes[0];
+const Triangle = Shapes[1];
+const Square = Shapes[2];
 
 inquirer
   .prompt([
@@ -32,35 +35,17 @@ inquirer
       console.error("Text too long! Please try again!");
       return;
     }
-    const makeShape = new Shapes(type, fill, letters, letterColor);
-    if (makeShape.type == "Circle") {
-      fs.writeFile(
-        "./examples/logo.svg",
-        `<svg version="1.1"
-        width="300" height="200"
-        xmlns="http://www.w3.org/2000/svg">
-
-     <circle cx="150" cy="100" r="80" fill="${makeShape.fill}" />
-
-     <text x="150" y="125" font-size="90" text-anchor="middle" fill="${makeShape.letterColor}">${makeShape.letters}</text>
-   
-   </svg>`,
-        function (err) {
-          err
-            ? console.log("Something went wrong! Let's try again.")
-            : console.log("Generated logo.svg");
-        }
-      );
-    } else if (makeShape.type == "Triangle") {
+    if (type == "Circle") {
+      const makeCircle = new Circle(fill, letters, letterColor);
       fs.writeFile(
         "./examples/logo.svg",
         `<svg version="1.1"
       width="300" height="200"
       xmlns="http://www.w3.org/2000/svg">
 
-     <polygon px= "50" points="50, 200 150, 0 300, 300" fill="${makeShape.fill}" />
- 
-   <text x="150" y="125" font-size="90" text-anchor="middle" fill="${makeShape.letterColor}">${makeShape.letters}</text>
+   <circle cx="150" cy="100" r="80" fill="${makeCircle.fill}" />
+
+   <text x="150" y="125" font-size="90" text-anchor="middle" fill="${makeCircle.letterColor}">${makeCircle.letters}</text>
  
  </svg>`,
         function (err) {
@@ -69,18 +54,38 @@ inquirer
             : console.log("Generated logo.svg");
         }
       );
-    } else if (makeShape.type == "Square") {
+    } else if (type == "Triangle") {
+      const makeTri = new Triangle(fill, letters, letterColor);
       fs.writeFile(
         "./examples/logo.svg",
         `<svg version="1.1"
-      width="300" height="200"
-      xmlns="http://www.w3.org/2000/svg">
- 
-   <rect x="50"  width="200" height="200" fill="${makeShape.fill}" />
+          width="300" height="200"
+          xmlns="http://www.w3.org/2000/svg">
 
-   <text x="150" y="125" font-size="90" text-anchor="middle" fill="${makeShape.letterColor}">${makeShape.letters}</text>
- 
- </svg>`,
+         <polygon px= "50" points="50, 200 150, 0 300, 300" fill="${makeTri.fill}" />
+
+       <text x="150" y="125" font-size="90" text-anchor="middle" fill="${makeTri.letterColor}">${makeTri.letters}</text>
+
+     </svg>`,
+        function (err) {
+          err
+            ? console.log("Something went wrong! Let's try again.")
+            : console.log("Generated logo.svg");
+        }
+      );
+    } else if (type == "Square") {
+      const makeSquare = new Square(fill, letters, letterColor);
+      fs.writeFile(
+        "./examples/logo.svg",
+        `<svg version="1.1"
+          width="300" height="200"
+          xmlns="http://www.w3.org/2000/svg">
+
+       <rect x="50"  width="200" height="200" fill="${makeSquare.fill}" />
+
+       <text x="150" y="125" font-size="90" text-anchor="middle" fill="${makeSquare.letterColor}">${makeSquare.letters}</text>
+
+     </svg>`,
         function (err) {
           err
             ? console.log("Something went wrong! Let's try again.")
